8ba6d3ebedc73c02bbd476d4be752094603b412c
ManishDevlikar <manish.devlikar5003@gmail.com>
2023-12-22 19:52:32 +0530

    create/use contex method 2

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import userContext from "./userContext";
+import { getCurrentUserDetail } from "../auth";
 function UserProvider({ children }) {
   const [user, setUsers] = React.useState({ name: "manish" });
+
+  useEffect(() => {
+    const user = getCurrentUserDetail();
+    setUsers(user);
+  }, []);
+
   return <userContext.Provider value={user}>{children}</userContext.Provider>;
 }
 
--- src/pages/user-routes/Profileinfo.jsx
+++ src/pages/user-routes/Profileinfo.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import Base from "../../components/Base";
+import userContext from "../../context/userContext";
 
 const Profileinfo = () => {
-    return (
-      <Base>
-        <div>Profile info</div>
-      </Base>
-    ); 
+  const user = useContext(userContext);
+  return (
+    <Base>
+      <div>Profile info {user?.name}</div>
+    </Base>
+  );
 };
 
 export default Profileinfo;
